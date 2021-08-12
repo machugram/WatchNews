18ca9673ebc96e899b93c9cc6ee2652263b578d5
Rexford Machu <rexfordmachu@gmail.com>
2021-08-12 21:27:36 +0000

    Initial Commit

---
+++ WatchNews WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchNews WatchKit Extension
+//
+//  Created by Rexford Machu on 8/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchNews WatchKit Extension/WatchNewsApp.swift
@@ -0,0 +1,19 @@
+//
+//  WatchNewsApp.swift
+//  WatchNews WatchKit Extension
+//
+//  Created by Rexford Machu on 8/12/21.
+//
+
+import SwiftUI
+
+@main
+struct WatchNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
